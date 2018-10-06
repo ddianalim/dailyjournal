f373f6cdf249ffc217ca1005aa178da2d78e9dd6
dianalimnos <dianalim1117@gmail.com>
2018-10-05 22:38:08 -0700

    bcrypt passwords added!

--- models/user.js
+++ models/user.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true }
+});
+
+UserSchema.pre('save', function(next) {
+  let user = this;
+
+  bcrypt.hash(user.password, 10, function (err, hash){
+    if (err) return next(err);
+
+    user.password = hash;
+    next();
+  })
 });
 
 const User = mongoose.model('User', UserSchema);
