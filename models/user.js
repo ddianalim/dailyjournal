084fb1e55f26938fa0ae5bb05f4c6eefca9e4ede
dianalimnos <dianalim1117@gmail.com>
2018-10-05 22:46:12 -0700

    user authenticate login method

--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
   username: { type: String, required: true },
@@ -18,6 +17,26 @@ UserSchema.pre('save', function(next) {
     next();
   })
 });
+
+UserSchema.statics.authenticate = function(username, password, next) {
+  User.findOne({ username: username })
+    .exec(function (err, user) {
+      if (err) {
+        return next(err)
+      } else if (!user) {
+        var err = new Error('User not found.');
+        err.status = 401;
+        return next(err);
+      }
+      bcrypt.compare(password, user.password, function (err, result) {
+        if (result === true) {
+          return next(null, user);
+        } else {
+          return next();
+        }
+      });
+    });
+}
 
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
