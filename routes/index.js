f085787bb9477458926af8327424cf475b299e25
dianalimnos <dianalim1117@gmail.com>
2018-10-05 22:50:43 -0700

    added Post login route

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,32 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const User = require('../models/user');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Daily Journal' });
+const router = express.Router();
+
+// home page
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'MakeReddit' });
 });
 
+// login
 router.get('/login', (req, res, next) => {
   res.render('login');
+});
+
+// POST login
+router.post('/login', (req, res, next) => {
+  User.authenticate(req.body.username, req.body.password, (err, user) => {
+    if (err || !user) {
+      const next_error = new Error("Username or password incorrect");
+      next_error.status = 401;
+
+      return next(next_error);
+    } else {
+      req.session.userId = user._id;
+
+      return res.redirect('/') ;
+    }
+  });
 });
 
 module.exports = router;
