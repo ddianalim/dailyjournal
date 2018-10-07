5a9452ef903ca707a247c86648c9f3482a77f7e0
dianalimnos <dianalim1117@gmail.com>
2018-10-06 23:47:46 -0700

    added date type

--- models/day.js
+++ models/day.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DaySchema = new Schema({
+  date: { type: Date, default: Date.now}
   description: { type: String, required: true },
 });
 
